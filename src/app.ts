fa19fd2b5a98b659f23f218f027b9ef039f1775f
Rodrigo <rodrigolemosrl@hotmail.com>
2020-11-21 18:35:21 -0300

    CHECKPOINT - initial structure finished

--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,7 @@ import express, { Request, Response } from 'express'
 const app: express.Application = express()
 
 app.get('/', (req: Request, res: Response) => {
-  return res.send({
-    message: 'hello world!'
-  })
+  return res.send({})
 })
 
 app.listen(3000, () => console.log('App running on port 3000...'))
