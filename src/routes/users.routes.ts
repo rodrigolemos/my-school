a0b60be2a3ef73d6aad128aaa6ddd13a3d1aeaf0
Rodrigo <rodrigolemosrl@hotmail.com>
2020-12-02 11:46:30 -0300

    Comment: defining routes rules

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -7,8 +7,10 @@ import UserController from '../controllers/UserController'
 
 const usersRouter = Router()
 
+// Users can be created from a public form
 usersRouter.post('/create', setUserFormat, handleRouteError, UserController.store)
 
+// Only logged in admins can view users
 usersRouter.use(ensureAuthentication)
 usersRouter.use(ensurePermission)
 
