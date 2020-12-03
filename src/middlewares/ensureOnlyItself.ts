23f9afcb8bc180df8cc4e0c19b1afaf786979cd3
Rodrigo <rodrigolemosrl@hotmail.com>
2020-12-03 14:26:47 -0300

    Code: ensure only itself middleware using body and url params

--- src/middlewares/ensureOnlyItself.ts
+++ src/middlewares/ensureOnlyItself.ts
@@ -2,7 +2,6 @@ import { Request, Response, NextFunction } from 'express'
 import AppError from '../utils/AppError'
 
 export async function confirmClientIdEqualsParameter(client_id: string, param_id: string): Promise<boolean> {
-  console.log(param_id)
   if (!client_id || !param_id)
     throw new AppError('Your id must be provided', 403)
 
@@ -17,8 +16,14 @@ export default async function ensureOnlyItself(request: Request, _: Response, ne
     if (typeof request.headers.client_id !== 'string')
       throw new AppError('Invalid client_id token', 401)
 
-    console.log(request.params)
-    if (!await confirmClientIdEqualsParameter(request.headers.client_id, request.params.id))
+    // GET, DELETE routes
+    let param_id = request.params.id
+
+    // POST, PUT, PATCH routes
+    if (!param_id)
+      param_id = request.body.id
+
+    if (!await confirmClientIdEqualsParameter(request.headers.client_id, param_id))
       throw new AppError('Unauthorized', 401)
 
     return next()
