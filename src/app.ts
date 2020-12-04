cafe45dfb4f63bc750c586ac090089df32c527eb
Rodrigo <rodrigolemosrl@hotmail.com>
2020-12-04 15:41:16 -0300

    CHECKPOINT - Step 1 finished

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,7 @@ app.use(cors())
 app.use(express.json())
 app.use(routes)
 
-app.use((err: Error, _request: Request, response: Response, _: NextFunction) => {
+app.use((err: Error, _req: Request, response: Response, _: NextFunction) => {
   console.error(err)
   if (err instanceof AppError) {
     return response.status(err.statusCode).json({
