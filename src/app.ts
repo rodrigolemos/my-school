09575b05468a504df30cb37e6bad7289f1147e89
Rodrigo <rodrigolemosrl@hotmail.com>
2020-12-12 11:18:05 -0300

    Config: add helmet, express-rate-limit and request size payload validation

--- src/app.ts
+++ src/app.ts
@@ -4,12 +4,28 @@ import 'express-async-errors'
 import AppError from './errors/AppError'
 import express, { Request, Response, NextFunction } from 'express'
 import cors from 'cors'
+import rateLimit from 'express-rate-limit'
+import helmet from 'helmet'
 import routes from './routes'
 
 const app: express.Application = express()
 
 app.use(cors())
-app.use(express.json())
+
+// It validates middleware config such as xss and referrer policy
+app.use(helmet())
+
+// It limits requests payload size
+app.use(express.json({ limit: '10kb' }))
+
+// It limits request quantity from the same IP in a period
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100
+})
+
+app.use(limiter)
+
 app.use(routes)
 
 app.use((err: Error, _req: Request, response: Response, _: NextFunction) => {
