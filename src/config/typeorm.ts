2213352ea2979f5304c5625875178ea7b7e21cee
Rodrigo <rodrigolemosrl@hotmail.com>
2021-03-08 21:28:52 -0300

    Config: tests connection

--- src/config/typeorm.ts
+++ src/config/typeorm.ts
@@ -1,3 +1,15 @@
-import { createConnection } from 'typeorm';
+import { createConnection, getConnectionOptions, Connection } from 'typeorm';
 
-createConnection()
+export default async (name = 'default'): Promise<Connection> => {
+  const defaultOptions = await getConnectionOptions();
+
+  return createConnection(
+    Object.assign(defaultOptions, {
+      name,
+      url:
+        name === 'test'
+          ? process.env.TEST_DATABASE_URL
+          : process.env.DATABASE_URL,
+    }),
+  );
+};
