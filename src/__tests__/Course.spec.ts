f9d7af1237070845a4e6f78a88b05a3648f8baf7
Rodrigo <rodrigolemosrl@hotmail.com>
2021-03-08 21:29:49 -0300

    Code: integration test

--- src/__tests__/Course.spec.ts
+++ src/__tests__/Course.spec.ts
@@ -1,8 +1,38 @@
-// import app from '../server'
+import request from 'supertest';
+import { Connection, getConnection } from 'typeorm';
+import createConnection from '../config/typeorm';
+
+import app from '../server'
+
+let connection: Connection;
 
 describe('Course tests', () => {
 
-  it('should return all courses', () => {
+  beforeAll(async () => {
+    connection = await createConnection('test');
+    await connection.runMigrations();
+  });
+
+  beforeEach(async () => {
+    await connection.query('DELETE FROM enrollments');
+    await connection.query('DELETE FROM courses');
+    await connection.query('DELETE FROM users');
+  });
+
+  afterAll(async () => {
+    const mainConnection = getConnection();
+
+    await connection.close();
+    await mainConnection.close();
+  });
+
+  it('should return all courses', async () => {
+    
+    const response = await request(app).get('/courses').send();
+
+    console.log(response.body);
+
+    expect(true).toBe(true);
     
   });
 
