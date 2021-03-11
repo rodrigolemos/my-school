250cb7bc4c013ead377a849b36faf7df9fb486c2
Rodrigo <rodrigolemosrl@hotmail.com>
2021-03-11 19:22:28 -0300

    Test: list courses

--- src/__tests__/Course.spec.ts
+++ src/__tests__/Course.spec.ts
@@ -1,4 +1,5 @@
 import request from 'supertest';
+import { v4 as uuidv4 } from 'uuid';
 import { Connection, getConnection } from 'typeorm';
 import { getTestConnection } from '../config/typeorm';
 
@@ -6,31 +7,65 @@ import app from '../server'
 
 let connection: Connection;
 
+const [courseId1, courseId2] = [uuidv4(), uuidv4()];
+const userId1 = uuidv4();
+const date = new Date().toISOString();
+
 describe('Course tests', () => {
 
   beforeAll(async () => {
     connection = await getTestConnection();
+    
     await connection.runMigrations();
-  });
 
-  beforeEach(async () => {
-    await connection.query('DELETE FROM enrollments');
-    await connection.query('DELETE FROM courses');
-    await connection.query('DELETE FROM users');
+    await connection.query(
+      `INSERT INTO users VALUES('${userId1}', 'Admin', 'email@email.com', 'password', 'admin', '${userId1}', '${date}')`
+    );
+
+    await connection.query(
+      `INSERT INTO courses VALUES('${courseId1}', 'Course 1', 'Desc 1', 'N', '${userId1}', '${date}', '${date}', 10)`
+    );
+
+    await connection.query(
+      `INSERT INTO courses VALUES('${courseId2}', 'Course 2', 'Desc 2', 'N', '${userId1}', '${date}', '${date}', 20)`
+    );
+
   });
 
   afterAll(async () => {
-    const mainConnection = getConnection();
+    await connection.query('TRUNCATE TABLE courses CASCADE');
+    await connection.query('TRUNCATE TABLE users CASCADE');  
 
+    const mainConnection = getConnection();
     await connection.close();
     await mainConnection.close();
   });
 
-  it('should return all courses', async () => {
-    
-    // const response = await request(app).get('/courses').send({});
+  it('should list all courses', async () => {
+    const response = await request(app).get('/courses').send();
 
-    expect(true).toBe(true);
+    expect(response.status).toBe(200);
+  });
+
+  it('should list a specific course', async () => {
+    const response = await request(app).get(`/courses/${courseId1}`).send();
+
+    expect(response.status).toBe(200);
+  });
+
+  it('should throw 404 if no courses were found', async () => {
+    
+    const response = await request(app).get(`/courses/${userId1}`).send();
+
+    expect(response.status).toBe(404);
+    
+  });
+
+  it('should throw 500 if course id is not an uuid', async () => {
+    
+    const response = await request(app).get(`/courses/123`).send();
+
+    expect(response.status).toBe(500);
     
   });
 
