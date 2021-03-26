7927abce9e45dfe7267e65e1a0673f3ebade8f65
Rodrigo <rodrigolemosrl@hotmail.com>
2021-03-26 19:13:11 -0300

    Test: should create a teacher

--- src/__tests__/CreateUserService.spec.ts
+++ src/__tests__/CreateUserService.spec.ts
@@ -1,11 +1,15 @@
 import request from 'supertest';
+import { v4 as uuidv4 } from 'uuid';
 import { Connection, getConnection } from 'typeorm';
 import { getTestConnection } from '../config/typeorm';
 import { generateHash } from '../config/hash'
+import { sign } from 'jsonwebtoken';
 
 import app from '../server'
 
 let connection: Connection;
+const userId1 = uuidv4();
+const date = new Date().toISOString();
 
 describe('CreateUserService', () => {
 
@@ -17,6 +21,12 @@ describe('CreateUserService', () => {
   afterAll(async () => {
     await connection.query('TRUNCATE TABLE users CASCADE');
 
+    const criptPassword = await generateHash('password')
+
+    await connection.query(
+      `INSERT INTO users VALUES('${userId1}', 'Admin', 'admin@email.com', '${criptPassword}', 'admin', '${userId1}', '${date}')`
+    );
+
     const mainConnection = getConnection();
     await connection.close();
     await mainConnection.close();
@@ -26,9 +36,31 @@ describe('CreateUserService', () => {
     const password = await generateHash('password')
     const response = await request(app).post('/users/create').send({
       name: 'Student',
-      email: 'email@email.com',
+      email: 'student@email.com',
       password,
       role: 'student'
+    });
+
+    expect(response.status).toBe(201);
+  });
+
+  it('should create a teacher', async () => {
+    const token = sign({}, process.env.JWT_SECRET || '', {
+      subject: String(userId1),
+      expiresIn: '1h'
+    })
+
+    const password = await generateHash('password')
+    const response = await request(app)
+    .post('/users/create')
+    .set({
+      'Authorization': `Bearer ${token}`
+    })
+    .send({
+      name: 'Teacher',
+      email: 'teacher@email.com',
+      password,
+      role: 'teacher'
     });
 
     expect(response.status).toBe(201);
